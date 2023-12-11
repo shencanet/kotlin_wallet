803eb57d20ab33cffff46f3fcf7a43556608ac38
shen <santi.dionis@gmail.com>
2023-12-11 20:56:36 +0100

    first commit

--- app/src/main/java/com/cristianvillamil/platziwallet/ui/home/HomeContract.kt
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/home/HomeContract.kt
@@ -1,5 +1,13 @@
 package com.cristianvillamil.platziwallet.ui.home
 
 interface HomeContrat {
+    interface View {
+        fun showLoader()
+        fun hideLoader()
+        fun showFavoriteTransfers(favoriteTransfer: List<FavoriteTransfer>)
+}
+
+interface Presentador {
     
+    fun retrieveFavoriteTransfers()
 }
